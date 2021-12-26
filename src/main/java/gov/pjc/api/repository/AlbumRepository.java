6eb65eea6863f66403415914d9f50f662f302d20
luiz-felipe <luizfelipe.freire@gmail.com>
2021-12-26 12:43:48 -0300

    add CrossOrigin

--- src/main/java/gov/pjc/api/repository/AlbumRepository.java
+++ src/main/java/gov/pjc/api/repository/AlbumRepository.java
@@ -2,9 +2,11 @@ package gov.pjc.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import gov.pjc.api.entity.Album;
 
+@CrossOrigin("*")
 @RepositoryRestResource(collectionResourceRel = "album", path = "album")
 public interface AlbumRepository extends JpaRepository<Album, Integer> {
 
--- src/main/java/gov/pjc/api/repository/ArtistaRespository.java
+++ src/main/java/gov/pjc/api/repository/ArtistaRespository.java
@@ -2,9 +2,11 @@ package gov.pjc.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import gov.pjc.api.entity.Artista;
 
+@CrossOrigin("*")
 @RepositoryRestResource(collectionResourceRel = "artista", path = "artista")
 public interface ArtistaRespository extends JpaRepository<Artista, Integer> {
 
